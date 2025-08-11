5c720d8b3bc434f0ef5c709d968228a4037dce8d
Noursaidd <nh20302@gmail.com>
2025-08-11 22:49:53 +0400

    corrections of the updates

--- Noorwebproject/app.py
+++ Noorwebproject/app.py
@@ -3,19 +3,8 @@ from flask import Flask, render_template
 application = Flask(__name__)
 
 @application.route('/')
-<<<<<<< HEAD
 def home():
     return render_template('index.html')
 
 if __name__ == '__main__':
     application.run(debug=True)
-
-
-## In python, they updated the app to application then app. is not accepted anymore ;)
-=======
-def hello_world():
-        return 'Hello, Noor web app!'
-
-if __name__ == '__main__':
-        application.run(debug=True)
->>>>>>> e5fdc87d8a8c2e88a171ea0ac245f6adfa7e1f80
